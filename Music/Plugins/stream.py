46382364290fe1d5959a58abfe57a0a17126a6d6
Ibay-ubot <101303882+Ibay-ubot@users.noreply.github.com>
2022-03-28 17:44:21 +0700

    Update stream.py

--- Music/Plugins/stream.py
+++ Music/Plugins/stream.py
@@ -195,7 +195,7 @@ async def vplay(c: Client, message: Message):
                 await loser.delete()
                 requester = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
                 await message.reply_photo(
-                    photo="cache/IMG_20211230_211039_090.jpg",
+                    photo="cache/IMG_20220328_172215.jpg",
                     caption=f"""
 💡 **Trek ditambahkan ke antrian**
 
@@ -226,7 +226,7 @@ async def vplay(c: Client, message: Message):
                 await loser.delete()
                 requester = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
                 await message.reply_photo(
-                    photo="cache/IMG_20211230_211039_090.jpg",
+                    photo="cache/IMG_20220328_172215.jpg",
                     caption=f"""
 ▶️ **Streaming video dimulai**
 
@@ -267,7 +267,7 @@ async def vplay(c: Client, message: Message):
                         await loser.delete()
                         requester = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
                         await message.reply_photo(
-                            photo="cache/IMG_20211230_211039_090.jpg",
+                            photo="cache/IMG_20220328_172215.jpg",
                             caption=f"""
 💡 **Trek ditambahkan ke antrian**
 
@@ -294,9 +294,9 @@ async def vplay(c: Client, message: Message):
                             add_to_queue(chat_id, songname, ytlink, url, "Video", Q)
                             await loser.delete()
                             requester = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
-                            thumb ="cache/IMG_20211230_165039_159.jpg"
+                            thumb ="cache/IMG_20220328_171923.jpg"
                             await message.reply_photo(
-                                photo="cache/IMG_20211230_211039_090.jpg",
+                                photo="cache/IMG_20220328_172215.jpg",
                                 caption=f"""
 ▷ **Memutar video dimulai**
 
